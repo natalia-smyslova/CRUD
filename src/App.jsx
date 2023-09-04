c33e0022c883a93eb8797327304c9f1c9601bdf4
Smyslova Natalia <madlynx@lenta.ru>
2023-09-04 20:42:27 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Note from './Components/Note'
+import Button from './Components/Button'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const url = 'http://localhost:7070/';
+
+  const [notes, setNotes] = useState([]);
+
+  const [newNote, setNewNote] = useState('');
+
+  const updateNotes = () => fetch(url + '/notes')
+  .then(res => res.json())
+  .then(json => setNotes(json));
+
+useEffect(() => updateNotes, []);
+
+  const onChange = (e) => setNewNote(e.target.value);
+
+  const addNote = async (e) => {
+    e.preventDefault();
+    await fetch(url + '/notes', {
+      method: 'POST',
+      body: newNote,
+    });
+    await updateNotes();
+  };
+
+
+  // const addNote = async (e) => {
+  //   e.preventDefault();
+
+  //   const request = fetch(url + '/notes', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(newNote),
+  //   });
+
+  //   const result = await request;
+  //   const json = await result.json();
+  //   console.log(json);
+  //   const status = json.status;
+  //   console.log(status);
+  // };
+
+  const deleteNote = async (id) => {
+    const request = fetch(url + '/notes/${id}', {
+      method: 'DELETE',
+    })
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='notes'>
+
+      <header className='header'>
+        <h1 className='header__title'>Notes</h1>
+        <Button element='header' icon="&#8634;" color="#25df25" fontSize="1.5rem" callback={updateNotes} />
+      </header>
+
+      <ul className='notes__list'>
+        {notes.map((note, id) => <li className='note' key={id}>
+          <Note note={note} deleteNote={deleteNote} />
+        </li>)}
+      </ul>
+
+      <footer className='footer'>
+        <h4 className='footer__title'>New Note</h4>
+        <form className='footer__form' onSubmit={addNote}>
+          <textarea className='footer__text' onChange={onChange}></textarea>
+          <Button element='footer' icon="&#10148;" color="black" fontSize="1.5rem" />
+        </form>
+      </footer>
+
+    </div>
   )
 }
 
